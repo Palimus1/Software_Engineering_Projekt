c8d502a7fdd46e8e7676dc3fe3d76b554056d6a4
paul.boos <paul.boos@uni-konstanz.de>
2026-03-31 15:12:25 +0200

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,22 @@
+
+
+@main def main(): Unit = {
+  println("Spielfeld wird geladen...")
+  val spielfeld = {
+    """
+    +---+---+---+
+    |   |   |   |
+    +---+---+---+
+    |   |   |   |
+    +---+---+---+
+    """
+  }
+  println(spielfeld)
+}
+
+
+
+
+
+
+
